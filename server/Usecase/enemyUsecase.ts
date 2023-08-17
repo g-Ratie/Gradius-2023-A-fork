ad26981a94005976ef881706e8a973e4ce6ecc94
1F10220146 <s1f102201469@iniad.org>
2023-08-17 14:51:27 +0900

    Revert "hotfix: 初期値を修正"
    This reverts commit 71e8a9d1b39fe57588b22c1f264d7f1359b173f3.

--- server/Usecase/enemyUsecase.ts
+++ server/Usecase/enemyUsecase.ts
@@ -23,7 +23,7 @@ export const enemyUsecase = {
 
 // 仮初期値
 export const enemyInfo = {
-  enemyFirstPos_x: 2000,
+  enemyFirstPos_x: 100,
   enemySpeed: 5,
   enemyRadius: 20,
   enemyHp: 10,
@@ -32,7 +32,7 @@ export const enemyInfo = {
 };
 
 setInterval(() => {
-  createEnemy();
+  create_enemy();
   deleteOffScreenEnemy();
 }, enemyInfo.makeEnemyFrequency);
 
@@ -41,11 +41,11 @@ setInterval(() => {
   moveEnemy();
 }, 10);
 
-const createEnemy = async () => {
+const create_enemy = async () => {
   const newEnemy: EnemyModel = {
     id: EnemyIdParser.parse(randomUUID()),
     pos: { x: enemyInfo.enemyFirstPos_x, y: Math.floor(Math.random() * 690) + 1 },
-    speed: enemyInfo.enemySpeed,
+    speed: enemyInfo.enemyHp,
     hp: enemyInfo.enemyHp,
     radius: enemyInfo.enemyRadius,
     type: 2,
